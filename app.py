327c437d4db37600a788b7ab578ef2f8fae151a8
Chase Dillard <c.dillard@protonmail.com>
2024-06-12 11:30:17 -0400

    Add initial streamlit setup

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import sqlite3
+
+
+st.write("Hello, World!")
+
+
+@st.cache_resource
+def init_connection():
+    return sqlite3.connect('hazmat.db')
+
+
+conn = init_connection()
+
+page = st.sidebar.selectbox('Page', ('Search', 'A', 'B', 'C'))
