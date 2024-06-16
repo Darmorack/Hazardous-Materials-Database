4970c607a0865da90d31619a60eb501f852a02e0
Colby Wentlandt <colbywentlandt@gmail.com>
2024-06-15 23:04:45 -0400

    Basic pages

---
+++ pages/🔄️_Update.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import sqlite3
+import helpers.sidebar
+
+
+st.set_page_config(
+    page_title="Hazardous Materials Database",
+    page_icon="🔄",
+    layout="wide"
+)
+
+helpers.sidebar.show()
+
+
+@st.cache_resource
+def init_connection():
+    return sqlite3.connect('Hazardous Materials Database.db')
+
+
+conn = init_connection()
+
+st.write("Update records in the database")
+
+user_input = st.text_input("Enter some text")
+
---
+++ pages/🗑️_Delete.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import sqlite3
+import helpers.sidebar
+
+st.set_page_config(
+    page_title="Hazardous Materials Database",
+    page_icon="🗑️",
+    layout="wide"
+)
+
+helpers.sidebar.show()
+
+
+@st.cache_resource
+def init_connection():
+    return sqlite3.connect('Hazardous Materials Database.db')
+
+
+conn = init_connection()
+
+st.write("Delete a record from the database")
+
+user_input = st.text_input("Enter some text")
+
